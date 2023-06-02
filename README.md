d74ed857bb624c2ad13a6a4fbc1cf758688ef894
nicolay <sirghiinicolai1@gmail.com>
2023-06-02 18:31:11 +0200

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# My_goall_app
-This is going to be an app that will help me to organize my time in order to go towards my goals more easy and more efficiently .
---
+++ script.js
@@ -0,0 +1,5 @@
+const contianer = document.querySelector(".container");
+
+const par = document.createElement("p")
+par.innerText  = "Hallo World";
+contianer.append(par);
