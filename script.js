49d39d754411f3cc8547cce614036d6a3935fec1
nicolay <sirghiinicolai1@gmail.com>
2023-06-08 21:53:21 +0200

    added learn part

--- script.js
+++ script.js
@@ -9,16 +9,14 @@ const dataArray = [
         id:1,
         title: "Statistics",
         do: ()=>{
-            
+//..........................................................................            
             contianer.innerHTML = "";
-            const statisticContainer = document.createElement("div");
-            const statistic_inside = document.createElement("div");
-            const description = document.createElement("div");
-            
-            const par = document.createElement("p");
-            const back = document.createElement("p");
+
+            const statisticContainer =        document.createElement("div");
+            const statistic_inside =          document.createElement("div");
+            const description =               document.createElement("div");
+            const back =                      document.createElement("p");
             back.innerText = "back";
-            par.innerText = "Statistics";
 
             back.classList.add("statisticsBackButton");
             statisticContainer.classList.add("statisticsContainer");
@@ -26,8 +24,90 @@ const dataArray = [
             description.classList.add("descritionWindow");
             
             back.addEventListener("click",dataArray[0].inner[0].do);
+//..........................................................................
+            dataArray[0].descriptions_data.forEach(el => {
+                const descriptionParts = document.createElement("div");
+                const par = document.createElement("p");
+                const color = document.createElement("div");
+                descriptionParts.classList.add("descriptionParts");
+                par.classList.add("description_par");
+                color.classList.add("descriotion_color");
+                par.innerText = el.title;
+                color.style.backgroundColor = el.color;
+                color.addEventListener("click",el.counting_function)
 
-            const descriptionData = [
+                descriptionParts.append(par,color)
+
+                description.append(descriptionParts);
+            });
+//..........................................................................
+            const position_of_proceses = ()=>{
+                dataArray[0].statisticArray.forEach(el => {
+              const beginingSleap = (el.sleap[0]*60)+(el.sleap[1]);
+              const endSleap = (el.sleap[2]*60)+(el.sleap[3]);
+              const beginingSleap2 = (el.sleap2[0]*60)+(el.sleap2[1]);
+              const endSleap2 = (el.sleap2[2]*60)+(el.sleap2[3]);
+              const beginingWork1 = (el.work1[0]*60)+(el.work1[1]);
+              const endWork1 = (el.work1[2]*60)+(el.work1[3]);
+              const beginingWork2 = (el.work2[0]*60)+(el.work2[1]);
+              const endWork2 = (el.work2[2]*60)+(el.work2[3]);
+
+              if((beginingSleap+endSleap)>0){
+                const sleap = document.createElement("div");
+                sleap.style.gridColumn = `${beginingSleap}/${endSleap}`;
+                sleap.style.gridRow = `${el.id}/${el.id+1}`;
+                sleap.classList.add("sleap");
+                statistic_inside.append(sleap);
+              };
+              if((beginingSleap2+endSleap2)>0){
+                const sleap = document.createElement("div");
+                sleap.style.gridColumn = `${beginingSleap}/${endSleap}`;
+                sleap.style.gridRow = `${el.id}/${el.id+1}`;
+                sleap.classList.add("sleap");
+                statistic_inside.append(sleap);
+              };
+              if((beginingWork1+endWork1)>0){
+               const work1 = document.createElement("div");
+               work1.style.gridColumn = `${beginingWork1}/${endWork1}`;
+               work1.style.gridRow = `${el.id}/${el.id+1}`;
+               work1.classList.add("work1");
+               statistic_inside.append(work1);
+              };
+              if((beginingWork2+endWork2)>0){
+              const work2 = document.createElement("div");
+              work2.style.gridColumn = `${beginingWork2}/${endWork2}`;
+              work2.style.gridRow = `${el.id}/${el.id+1}`;
+              work2.classList.add("work2");
+              statistic_inside.append(work2);
+              };
+             });};position_of_proceses();
+//..........................................................................             
+            statisticContainer.append(back,description, statistic_inside);
+            contianer.append(statisticContainer);
+//..........................................................................            
+        },
+        inner: [
+            {
+                id:1,
+                do: ()=>{
+                    contianer.innerHTML = "";
+                    const first_page = document.createElement("div");
+                    first_page.classList.add("first_page");
+                    dataArray.forEach(el=>{
+                    const btn = document.createElement("div");
+                    btn.innerText = el.title;
+                    btn.style.gridRow = el.styles[0].row;
+                    btn.style.gridColumn = el.styles[0].column;
+                    btn.addEventListener("click",el.do);
+                    first_page.append(btn);
+                });
+                contianer.append(first_page);
+                },
+               
+            },
+            
+        ],
+        descriptions_data: [
                 {
                     id:1,
                     title: "Sleep",
@@ -73,9 +153,6 @@ const dataArray = [
                         contianer.append(back,descriptionContainer);
                         back.addEventListener("click",dataArray[0].do);
     
-                        
-                        
-    
                         const work_form_counting = (e)=>{
                             e.preventDefault();
                             let from = 0;
@@ -90,21 +167,16 @@ const dataArray = [
                              };});
                             dataArray[0].statisticArray.forEach(el => {
                                 const sleap1 = (((el.sleap[2]*60)+el.sleap[3])-((el.sleap[0]*60)+el.sleap[1]))/60;
-    
-                
-                                
+                                const sleap2 = (((el.sleap2[2]*60)+el.sleap2[3])-((el.sleap2[0]*60)+el.sleap2[1]))/60;
+                                const total = sleap1+sleap2;
                                 if(el.id >= from && el.id <= to){
-                                  amount = amount + sleap1;  
+                                  amount = amount + total;
                                 }
                             });
-                         
                             informHours.innerText = `Total hours of sleap:${amount}`;
-                
-                            
                             e.target.reset();
                             
                         };
-                        
                         Form.addEventListener("submit",work_form_counting);
                         }
                 },
@@ -193,8 +265,75 @@ const dataArray = [
                     title: "learn",
                     color: "blue",
                     counting_function: ()=>{
-                        console.log("learn");
-                    }
+                        contianer.innerHTML = "";
+                       
+                        //============================================
+                        //                   FORM                   //
+                        //============================================
+                        const Form = document.createElement("form");
+                        const beginingInput = document.createElement("input");
+                        const endInput = document.createElement("input");
+                        const btnForm = document.createElement("button");
+                        btnForm.innerText = "check";
+                        
+                        beginingInput.type = "text";
+                        beginingInput.placeholder = "begining";
+                        beginingInput.name = "begining";
+                        endInput.type = "text";
+                        endInput.placeholder = "end";
+                        endInput.name = "end";
+                        Form.append(beginingInput,endInput,btnForm);
+                        Form.classList.add("statisticForm");
+    
+                        //============================================
+                        //                   FORM                   //
+                        //============================================
+                        const back = document.createElement("div");
+                        const descriptionContainer = document.createElement("div");
+                        //..................................................
+                        const informHours = document.createElement("p");
+                        
+                        informHours.innerText = `Total free hours:`;
+                        
+                        informHours.classList.add("total_hours_par");
+                        
+                        //..................................................
+                        back.innerText = "back";
+                        back.classList.add("statisticsBackButton");
+                        descriptionContainer.classList.add("description_work_container");
+                        descriptionContainer.append(Form,informHours)
+                        contianer.append(back,descriptionContainer);
+                        back.addEventListener("click",dataArray[0].do);
+    
+                        const work_form_counting = (e)=>{
+                            e.preventDefault();
+                            let from = 0;
+                            let to = 0;
+                            let amount = 0;
+                            dataArray[0].statisticArray.forEach(el => {
+                             if(el.date === e.target.begining.value){
+                                from = el.id
+                             };
+                             if(el.date === e.target.end.value){
+                                to = el.id
+                             };});
+                            dataArray[0].statisticArray.forEach(el => {
+                                const sleap1 = (((el.sleap[2]*60)+el.sleap[3])-((el.sleap[0]*60)+el.sleap[1]))/60;
+                                const sleap2 = (((el.sleap2[2]*60)+el.sleap2[3])-((el.sleap2[0]*60)+el.sleap2[1]))/60;
+                                const work1 = (((el.work1[2]*60)+el.work1[3])-((el.work1[0]*60)+el.work1[1]))/60;
+                                const work2 = (((el.work2[2]*60)+el.work2[3])-((el.work2[0]*60)+el.work2[1]))/60;
+                                const total = 24-(sleap1+sleap2+work1+work2);
+                                if(el.id >= from && el.id <= to){
+                                  amount = amount + total;
+                                }
+                            });
+                            informHours.innerText = `Total free hours:${amount}`;
+                            e.target.reset();
+                            
+                        };
+                        Form.addEventListener("submit",work_form_counting);
+                        }
+                    
                 },
                 {
                     id:4,
@@ -212,78 +351,7 @@ const dataArray = [
                         console.log("buy");
                     }
                 }
-            ];
-             
-             descriptionData.forEach(el => {
-                const descriptionParts = document.createElement("div");
-                const par = document.createElement("p");
-                const color = document.createElement("div");
-                descriptionParts.classList.add("descriptionParts");
-                par.classList.add("description_par");
-                color.classList.add("descriotion_color");
-                par.innerText = el.title;
-                color.style.backgroundColor = el.color;
-                color.addEventListener("click",el.counting_function)
-
-                descriptionParts.append(par,color)
-
-                description.append(descriptionParts);
-            });
-
-
-            statisticContainer.append(back,par,description, statistic_inside);
-            contianer.append(statisticContainer);
-            
-
-            const position_of_sleap = ()=>{
-                dataArray[0].statisticArray.forEach(el => {
-              const beginingSleap = (el.sleap[0]*60)+(el.sleap[1]);
-              const endSleap = (el.sleap[2]*60)+(el.sleap[3]);
-              const beginingWork1 = (el.work1[0]*60)+(el.work1[1]);
-              const endWork1 = (el.work1[2]*60)+(el.work1[3]);
-              const beginingWork2 = (el.work2[0]*60)+(el.work2[1]);
-              const endWork2 = (el.work2[2]*60)+(el.work2[3]);
-              
-              const sleap = document.createElement("div");
-              const work1 = document.createElement("div");
-              const work2 = document.createElement("div");
-              sleap.style.gridColumn = `${beginingSleap}/${endSleap}`;
-              work1.style.gridColumn = `${beginingWork1}/${endWork1}`;
-              work2.style.gridColumn = `${beginingWork2}/${endWork2}`;
-              sleap.innerText = el.date;
-
-              sleap.classList.add("sleap");
-              work1.classList.add("work1");
-              work2.classList.add("work2");
-
-              sleap.addEventListener("click",dataArray[0].inner[0].click_in_sleap)
-              statistic_inside.append(work1,sleap,work2); 
-            });
-
-            };position_of_sleap();
-        },
-        inner: [
-            {
-                id:1,
-                do: ()=>{
-                    contianer.innerHTML = "";
-                    const first_page = document.createElement("div");
-                    first_page.classList.add("first_page");
-                    dataArray.forEach(el=>{
-                    const btn = document.createElement("div");
-                    btn.innerText = el.title;
-                    btn.style.gridRow = el.styles[0].row;
-                    btn.style.gridColumn = el.styles[0].column;
-                    btn.addEventListener("click",el.do);
-                    first_page.append(btn);
-                });
-                contianer.append(first_page);
-                },
-               
-            },
-            
-        ],
-        description_form_functions: [],
+            ],
         styles: [
         {
             id:1,
@@ -302,6 +370,7 @@ const dataArray = [
                 id:1,
                 date: "04,06,2023",
                 sleap:[07,01,13,00],
+                sleap2:[0,0,0,0],
                 work1: [0,1,06,0],
                 work2: [21,0,24,0]
             },
@@ -309,6 +378,7 @@ const dataArray = [
                 id:2,
                 date: "05,06,2023",
                 sleap:[6,5,11,11],
+                sleap2:[0,0,0,0],
                 work1: [0,1,5,15],
                 work2: [20,30,24,0]
             },
@@ -316,6 +386,7 @@ const dataArray = [
                 id:3,
                 date: "06,06,2023",
                 sleap:[8,15,11,55],
+                sleap2:[0,0,0,0],
                 work1: [0,1,7,10],
                 work2: [20,30,24,0]
             },
@@ -323,6 +394,7 @@ const dataArray = [
                 id:4,
                 date: "07,06,2023",
                 sleap:[6,45,12,45],
+                sleap2:[0,0,0,0],
                 work1: [0,1,6,5],
                 work2: [22,0,24,0]
             },
@@ -330,8 +402,17 @@ const dataArray = [
                 id:5,
                 date: "08,06,2023",
                 sleap:[6,33,13,9],
+                sleap2:[0,0,0,0],
                 work1: [0,1,5,30],
-                work2: [0,1,0,2]
+                work2: [0,0,0,0]
+            },
+            {
+                id:6,
+                date: "09,06,2023",
+                sleap:[0,0,0,0],
+                sleap2:[0,0,0,0],
+                work1: [0,0,0,0],
+                work2: [0,0,0,0]
             }
 
         ]
